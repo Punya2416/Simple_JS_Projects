706d1c053ec6f51294fb04c4b5813b650412899f
Punya M <122287693+Punya2416@users.noreply.github.com>
2024-03-23 21:31:14 +0530

    9.ReturnNegative.js

---
+++ 9.ReturnNegative.js
@@ -0,0 +1,7 @@
+function returnNegative(number) {
+    return number < 0 ? number : -number;
+}
+
+// Example usage:
+let num = 125;
+console.log("Negative value of", num, "is", returnNegative(num));
